adb7ec5637f6c19a2119da675da86a971298077e
cwnidog <cwnidog@comcast.net>
2015-01-19 19:41:22 -0800

    Adding comments

--- GitClient/SearchRepositoriesViewController.swift
+++ GitClient/SearchRepositoriesViewController.swift
@@ -14,13 +14,14 @@ class SearchRepositoriesViewController: UIViewController, UITableViewDataSource,
   
   @IBOutlet weak var searchBarOutlet: UISearchBar!
     
-  var repositories = [Repository]()
+  var repositories = [Repository]() // the array of repositories satisfying the search criteria - may be empty
   let networkController = NetworkController()
 
   
     override func viewDidLoad() {
       super.viewDidLoad()
       
+      // we're our own delegate for the tableView and searchBar
       self.tableViewOutlet.dataSource = self
       self.tableViewOutlet.delegate = self
       self.searchBarOutlet.delegate = self
@@ -41,18 +42,21 @@ class SearchRepositoriesViewController: UIViewController, UITableViewDataSource,
   
   func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCellWithIdentifier("REPO_CELL", forIndexPath: indexPath) as UITableViewCell
-    cell.textLabel?.text = self.repositories[indexPath.row].name
+    cell.textLabel?.text = self.repositories[indexPath.row].name // all we do for now is print the name of the repository
     return cell
   }
   
   func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-    println("tableView delegate called.")
+    println("tableView delegate called.") // place holder
   }
   
   //MARK: UISearchBarDelegate
   
+  // when we click on the search bar's search button get the repositories that satisfy the search term
   func searchBarSearchButtonClicked(searchBar: UISearchBar) {
     println(searchBar.text)
+    
+    // call fetchRepositoriesForSearchTerm to send a request to the Git server for repositories
     self.networkController.fetchRepositoriesForSearchTerm(searchBar.text, callback: { (repositories, errorDescription) -> (Void) in
       if errorDescription == nil {
         self.repositories = repositories!
--- NetworkViewController.swift
+++ NetworkViewController.swift
@@ -22,23 +22,26 @@ class NetworkController {
     let dataTask = self.urlSession.dataTaskWithURL(url!, completionHandler: { (data,
       urlResponse, error) -> Void in
       if error == nil {
+        // cast down the url as a NSHTTPURLResponse, so we can get the response's status code
         if let httpResponse = urlResponse as? NSHTTPURLResponse {
           switch httpResponse.statusCode {
-          case 200 ... 299:
-          if let jsonDictionary = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: nil) as? [String : AnyObject] {
-            var repositories = [Repository]()
-            var itemArray = jsonDictionary["items"] as NSArray
-            for item in itemArray {
-              if let dictionary = item as? [String : AnyObject]{
-                var repository = Repository(jsonDictionary: dictionary)
-                repositories.append(repository)
-              } // if let
-            } // for item
-            // put a request for the Search Repository Menu Table View Controller to reload its data
-            NSOperationQueue.mainQueue().addOperationWithBlock({ () -> Void in
-              callback(repositories, nil)
-            })
-            } // if let httpResponse
+          case 200 ... 299: // good response
+            // serialize the raw response data into a JSON dictionary
+            if let jsonDictionary = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: nil) as? [String : AnyObject] {
+              
+              var repositories = [Repository]() // will hold set of repositories from teh JSON dictionary
+              var itemArray = jsonDictionary["items"] as NSArray // the root dictionary can hold more than one items dictionary
+              for item in itemArray {
+                if let dictionary = item as? [String : AnyObject]{
+                  var repository = Repository(jsonDictionary: dictionary) // pull out what we want to display
+                  repositories.append(repository) // add it to the list
+                } // if let
+              } // for item
+              // put a request for the Search Repository Menu Table View Controller to reload its data
+              NSOperationQueue.mainQueue().addOperationWithBlock({ () -> Void in
+                callback(repositories, nil)
+              })
+              } // if let httpResponse
             
           case 400 ... 499:
           println("Got response saying error at our end.")
