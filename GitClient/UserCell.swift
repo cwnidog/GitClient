df6fd3c2a612273717e8061e4db456e3aed1a4b7
John Leonard <cwnidog1@gmail.com>
2015-01-21 16:49:03 -0800

    Adding code to show user photos in a User detail VC

---
+++ GitClient/UserCell.swift
@@ -0,0 +1,14 @@
+//
+//  UserCell.swift
+//  GitClient
+//
+//  Created by John Leonard on 1/21/15.
+//  Copyright (c) 2015 John Leonard. All rights reserved.
+//
+
+import UIKit
+
+class UserCell: UICollectionViewCell {
+  @IBOutlet weak var imageView: UIImageView!
+  
+} // UserCell
